927d3779c45015964979c255d0b7440d58b650f3
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-01 18:09:37 +0000

    add marketing layout; page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,5 @@
+import { buttonVariants } from '@/components/ui/button'
+import { Dashboard, Login } from '@/icons'
 import Link from 'next/link'
 
 interface MarketingLayoutProps {
@@ -8,16 +10,24 @@ export default function MarketingLayout({ children }: MarketingLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="border-b">
-        <div className="flex container px-56 mx-auto h-16 items-center justify-between">
-          <Link href="/login" className={'font-mono font-bold'}>
+        <div className="flex max-w-screen-lg px-4 mx-auto h-16 items-center justify-between">
+          <Link
+            href="/"
+            className={'font-bold text-xl flex items-center gap-2'}
+          >
             DashCMS
+            <Dashboard className="w-5 h-5" />
           </Link>
-          <Link href="/login" className={''}>
+          <Link
+            href={'/login'}
+            className={buttonVariants({ variant: 'default' })}
+          >
             Login
+            <Login className="w-4 h-4 ml-2" />
           </Link>
         </div>
       </header>
-      <main className="flex-1">{children}</main>
+      <main>{children}</main>
     </div>
   )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,23 @@
+import { buttonVariants } from '@/components/ui/button'
+import { Collaborate, Github, Widgets, Data } from '@/icons'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
+
+interface FeatureProps {
+  title: string
+  description: string
+  icon: JSX.Element
+}
+
+function Feature({ title, description, icon }: FeatureProps) {
+  return (
+    <div className="rounded-lg border p-6 hover:shadow-md">
+      {icon}
+      <h4 className="font-semibold mb-4 mt-2">{title}</h4>
+      <p className="text-sm">{description}</p>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -8,54 +27,48 @@ export default function Home() {
           <h1 className="font-extrabold text-5xl leading-tight mb-5">
             Build dashboards in seconds!
           </h1>
-          <p className="text-lg text-slate-600 leading-normal text-muted-foreground sm:leading-8">
-            I&apos;m building a web app with Next.js 13 and open sourcing
-            everything. Follow along as we figure this out together.
+          <p className="text-lg text-slate-600 text-muted-foreground sm:leading-8">
+            Empower Your Insights, Customize Your View: Your Data, Your
+            Dashboards
           </p>
-          {/* <div className="space-x-4">
-            <Link href="/login" className={cn(buttonVariants({ size: 'lg' }))}>
+          <div className="space-y-4">
+            <Link
+              href="/login"
+              className={cn(buttonVariants({ size: 'sm' }), 'mr-2')}
+            >
               Get Started
             </Link>
             <Link
-              href={siteConfig.links.github}
+              href="https://github.com/rawat9/ug-project"
               target="_blank"
               rel="noreferrer"
-              className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
+              className={buttonVariants({ variant: 'outline', size: 'sm' })}
             >
               GitHub
+              <Github className="w-4 h-4 ml-2" />
             </Link>
-          </div> */}
+          </div>
         </div>
+
         <h2 className="font-semibold text-xl">Features</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6">Typescript</h4>
-            <p className="text-sm">Description</p>
-          </div>
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6">Angular</h4>
-            <p className="text-sm">Description</p>
-          </div>
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4">
+          <Feature
+            title="Data Integration"
+            description="Connect to the data sources of your choice"
+            icon={<Data className="w-12 h-12" />}
+          />
 
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6"></h4>
-            <p className="text-sm">Description</p>
-          </div>
+          <Feature
+            title="Templates and Widgets"
+            description="A library of customizable widgets for quick dashboard creation"
+            icon={<Widgets className="w-12 h-12" />}
+          />
 
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6"></h4>
-            <p className="text-sm">Description</p>
-          </div>
-
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6"></h4>
-            <p className="text-sm">Description</p>
-          </div>
-
-          <div className="rounded-lg border p-6">
-            <h4 className="font-semibold mb-6"></h4>
-            <p className="text-sm">Description</p>
-          </div>
+          <Feature
+            title="Sharing and Collaboration"
+            description="Share dashboards securely and collaborate with team members"
+            icon={<Collaborate className="w-12 h-12" />}
+          />
         </div>
       </section>
     </>
