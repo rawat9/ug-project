a38b1559c873a3ca22c67da9f4c94ea045d9d050
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-17 22:41:18 +0000

    move layout.tsx into page.tsx as breaking out of nested route isn't possible

--- app/dashboard/[slug]/layout.tsx
+++ app/dashboard/[slug]/layout.tsx
@@ -1,3 +1,7 @@
-export default function Layout() {
-  return <div>Dashboard detail layout</div>
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return <main>{children}</main>
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,56 @@
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Add, Search } from '@/icons'
+import { Add } from '@/icons'
 import Link from 'next/link'
+import { Header } from '@/components/header'
+import { Search } from '@/components/search'
 
 export default function DashboardPage() {
+  const projectsCount = 1
+
   return (
     <>
-      <div className="flex flex-col max-w-screen-lg mx-auto px-4 pt-16 overflow-y-hidden">
-        <div className="flex items-center space-x-2">
-          <Search className="w-5 h-5" />
-          <Input type="search" placeholder="Search..." />
-          <Button>
-            <Add />
-            <p className="hidden sm:flex ml-2">Add New</p>
-          </Button>
-        </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
-          {Array.from({ length: 3 }).map((_, i) => (
-            <Link
-              href={`/dashboard/${i + 1}`}
-              aria-label="Open dashboard"
-              key={i}
-            >
-              <div className="rounded-lg border p-4 bg-white shadow-sm hover:shadow-md cursor-pointer min-h-[150px]">
-                <h4 className="font-semibold py-2">Dashboard {i + 1}</h4>
-                <p className="text-sm">This is an empty dashboard document</p>
+      <Header />
+      <main className="bg-zinc-50 min-h-[calc(100vh_-_4rem)]">
+        <div className="flex flex-col max-w-screen-lg mx-auto px-4 pt-16 overflow-y-hidden">
+          {projectsCount > 0 ? (
+            <>
+              <div className="flex items-center space-x-2">
+                <Search />
+                <Button>
+                  <Add />
+                  <p className="hidden sm:flex ml-2">Add New</p>
+                </Button>
               </div>
-            </Link>
-          ))}
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <Link
+                    href={`/dashboard/${i + 1}`}
+                    aria-label="Open dashboard"
+                    key={i}
+                  >
+                    <div className="rounded-lg border p-4 bg-white shadow-sm hover:shadow-md cursor-pointer min-h-[150px]">
+                      <h4 className="font-semibold py-2">Dashboard {i + 1}</h4>
+                      <p className="text-sm">
+                        This is an empty dashboard document
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className="flex flex-col w-8/12 place-self-center rounded-lg border min-h-[300px] bg-white justify-center items-center shadow-sm my-10">
+              <h2 className="text-2xl font-semibold text-center mb-2">
+                No projects yet!
+              </h2>
+              <Button>
+                <Add />
+                <p className="hidden sm:flex ml-2">Add New</p>
+              </Button>
+            </div>
+          )}
         </div>
-      </div>
+      </main>
     </>
   )
 }
