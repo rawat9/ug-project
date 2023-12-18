7180a3dc08d362e4ec7ae092210f314a8b44a13f
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-18 20:18:51 +0000

    add prettier-tailwindcss plugin; add empty-state ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { Add } from '@/icons'
 import Link from 'next/link'
 import { Header } from '@/components/header'
 import { Search } from '@/components/search'
+import Image from 'next/image'
 
 export default function DashboardPage() {
   const projectsCount = 1
@@ -10,46 +11,54 @@ export default function DashboardPage() {
   return (
     <>
       <Header />
-      <main className="bg-zinc-50 min-h-[calc(100vh_-_4rem)]">
-        <div className="flex flex-col max-w-screen-lg mx-auto px-4 pt-16 overflow-y-hidden">
-          {projectsCount > 0 ? (
-            <>
-              <div className="flex items-center space-x-2">
-                <Search />
-                <Button>
-                  <Add />
-                  <p className="hidden sm:flex ml-2">Add New</p>
-                </Button>
-              </div>
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
-                {Array.from({ length: 4 }).map((_, i) => (
-                  <Link
-                    href={`/dashboard/${i + 1}`}
-                    aria-label="Open dashboard"
-                    key={i}
-                  >
-                    <div className="rounded-lg border p-4 bg-white shadow-sm hover:shadow-md cursor-pointer min-h-[150px]">
-                      <h4 className="font-semibold py-2">Dashboard {i + 1}</h4>
-                      <p className="text-sm">
-                        This is an empty dashboard document
-                      </p>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </>
-          ) : (
-            <div className="flex flex-col w-8/12 place-self-center rounded-lg border min-h-[300px] bg-white justify-center items-center shadow-sm my-10">
-              <h2 className="text-2xl font-semibold text-center mb-2">
-                No projects yet!
-              </h2>
+      <main className="min-h-screen bg-zinc-50">
+        {projectsCount > 0 ? (
+          <div className="mx-auto flex max-w-screen-lg flex-col px-4 py-36">
+            <div className="flex items-center space-x-2">
+              <Search />
               <Button>
                 <Add />
-                <p className="hidden sm:flex ml-2">Add New</p>
+                <p className="ml-2 hidden sm:flex">Add New</p>
               </Button>
             </div>
-          )}
-        </div>
+            <div className="grid gap-4 py-10 md:grid-cols-2 lg:grid-cols-3">
+              {Array.from({ length: projectsCount }).map((_, i) => (
+                <Link
+                  href={`/dashboard/${i + 1}`}
+                  aria-label="Open dashboard"
+                  key={i}
+                >
+                  <div className="min-h-[150px] cursor-pointer rounded-lg border bg-white p-4 shadow-sm hover:shadow-md">
+                    <h4 className="py-2 font-semibold">Dashboard {i + 1}</h4>
+                    <p className="text-sm">
+                      This is an empty dashboard document
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="flex min-h-screen flex-col items-center justify-center">
+            <Image
+              src="/empty-dashboard.svg"
+              alt="Empty dashboard"
+              width={400}
+              height={300}
+            />
+            <h1 className="mb-2 text-2xl font-semibold">
+              Start building your dashboard!
+            </h1>
+            <h4 className={'mb-3 text-center text-sm text-gray-500'}>
+              <p>Every dashboard you create will appear here.</p>
+              To get started, click the button below
+            </h4>
+            <Button variant="ghost" aria-label="Create a new dashboard">
+              <Add />
+              <p className="ml-2 flex">Add New</p>
+            </Button>
+          </div>
+        )}
       </main>
     </>
   )
--- components/header.tsx
+++ components/header.tsx
@@ -4,9 +4,9 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export function Header() {
   return (
-    <header className="flex max-w-full px-4 h-16 items-center justify-between border-b">
-      <Link href="/" className={'font-bold text-xl flex items-center gap-2'}>
-        <Dashboard className="w-5 h-5" />
+    <header className="fixed flex h-16 w-full items-center justify-between border-b bg-white px-4">
+      <Link href="/" className={'flex text-xl font-bold'}>
+        <Dashboard className="h-5 w-5" />
       </Link>
       <Link href="/" className={'mr-auto pl-4'}>
         Integrations
