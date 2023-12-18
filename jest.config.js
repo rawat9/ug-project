2eccee8cb87db2b8aa01028c5ce24f2f07ea900c
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-18 12:02:37 +0000

    setup and configure jest, react/testing-library as unit testing lib

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
+}
+
+module.exports = createJestConfig(config)
