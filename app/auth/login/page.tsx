48e32f4ef91e7d8615775125d440e7c3702d5c48
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-19 20:00:39 +0000

    combine sign in & sign-up into one component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -26,21 +26,13 @@ export default function LoginPage() {
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
-            Welcome back
+            Sign up or log in
           </h1>
-          <p className="text-sm text-slate-500">
-            Enter your email to sign in to your account
+          <p className="text-sm text-slate-600">
+            Enter your email to log in or create an account
           </p>
         </div>
         <AuthForm />
-        <p className="px-8 text-center text-sm text-muted-foreground">
-          <Link
-            href={'/register'}
-            className="hover:text-brand underline underline-offset-4"
-          >
-            Don&apos;t have an account? Sign Up
-          </Link>
-        </p>
       </div>
     </div>
   )
