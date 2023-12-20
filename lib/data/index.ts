904eb465ca5314e7d156f3acfa493d61a963b7bd
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-20 19:32:41 +0000

    add Database types

---
+++ lib/data/index.ts
@@ -0,0 +1,38 @@
+import { createSupabaseBrowserClient } from '@/lib/supabase/client'
+import { Tables } from '@/types/database'
+
+type Dashboard = Tables<'dashboard'>
+
+export const fetchDashboards = async (): Promise<Dashboard[]> => {
+  const supabase = createSupabaseBrowserClient()
+  const { data, error } = await supabase
+    .from('dashboard')
+    .select()
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.error(error.message)
+    throw new Error('Error fetching data')
+  }
+
+  return data ?? []
+}
+
+export const filterDashboards = async (
+  searchTerm: string,
+): Promise<Dashboard[]> => {
+  const supabase = createSupabaseBrowserClient()
+  const { data, error } = await supabase
+    .rpc('search', {
+      search_term: searchTerm,
+    })
+    .select()
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.error(error.message)
+    throw new Error('Error fetching data')
+  }
+
+  return data ?? []
+}
