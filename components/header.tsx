0c54e0504ab380ca60420a339732fc770bec4452
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-20 19:31:43 +0000

    add Dropdown component for user details & logout

--- components/header.tsx
+++ components/header.tsx
@@ -1,22 +1,64 @@
 import Link from 'next/link'
 import { Dashboard } from '@/icons'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { createSupabaseBrowserClient } from '@/lib/supabase/client'
+import { Logout } from '@/components/logout'
 
-export function Header() {
+export async function Header() {
+  const supabase = createSupabaseBrowserClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
   return (
-    <header className="fixed flex h-16 w-full items-center justify-between border-b bg-white px-4">
+    <header className="flex h-16 w-full items-center justify-between border-b bg-white px-4">
       <Link href="/" className={'flex text-xl font-bold'}>
         <Dashboard className="h-5 w-5" />
       </Link>
       <Link href="/" className={'mr-auto pl-4'}>
         Integrations
       </Link>
-      <Link href={'/'}>
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>{'cn'}</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm leading-none">{user?.email}</p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Profile
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Settings
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <Logout />
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   )
 }
