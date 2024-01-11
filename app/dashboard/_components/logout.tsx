5d5ab83239e72b8cfde01a25e51fd355ad807c96
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-11 23:08:33 +0000

    convert from MenuItem to a Button

--- app/dashboard/_components/logout.tsx
+++ app/dashboard/_components/logout.tsx
@@ -1,21 +1,26 @@
 'use client'
 
-import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
 import { signOut } from '@/lib/actions'
 import { useRouter } from 'next/navigation'
+import { Logout as LogoutIcon } from '@/icons'
 
 export function Logout() {
   const router = useRouter()
 
   async function handleLogout() {
-    const { error } = await signOut()
-
-    if (error) {
-      console.error(error)
-    }
-
+    await signOut()
     router.push('/auth/login')
   }
 
-  return <DropdownMenuItem onClick={handleLogout}>Log out</DropdownMenuItem>
+  return (
+    <Button
+      className="w-full"
+      data-testid="logout-button"
+      onClick={handleLogout}
+    >
+      Log out
+      <LogoutIcon className="ml-1 h-4 w-4" />
+    </Button>
+  )
 }
