ca48afe20c2adcd81f5b521bd2d0287310af1ecc
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-13 08:30:15 +0000

    use monaco-editor

--- app/dashboard/[slug]/_components/editor/code-editor.tsx
+++ app/dashboard/[slug]/_components/editor/code-editor.tsx
@@ -1,20 +1,22 @@
 'use client'
 
-import CodeMirror from '@uiw/react-codemirror'
-import { StandardSQL, sql } from '@codemirror/lang-sql'
 import { disableGrammarly } from '@/lib/utils'
-
-const extensions = [sql({ dialect: StandardSQL })]
+import Editor, { OnMount } from '@monaco-editor/react'
 
 export function CodeEditor() {
   return (
-    <CodeMirror
+    <Editor
       height="200px"
-      extensions={extensions}
-      theme="light"
-      className="overflow-auto rounded-lg border border-gray-200 text-left shadow-sm"
-      placeholder={'Start typing your query...'}
-      onCreateEditor={() => disableGrammarly()}
+      language="sql"
+      theme="vs-light"
+      defaultValue="-- Write your query"
+      className="border-gray-200 text-lg"
+      options={{
+        fontSize: 15,
+        minimap: {
+          enabled: false,
+        },
+      }}
     />
   )
 }
