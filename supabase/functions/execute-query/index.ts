20ba2d3e7ab72b5d900694835126768f6a116de8
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-17 11:24:20 +0000

    update port to supabase connection pool port

--- supabase/functions/execute-query/index.ts
+++ supabase/functions/execute-query/index.ts
@@ -2,7 +2,10 @@ import postgres from 'postgres'
 import { Logger } from 'logger'
 
 const sql = postgres(
-  'postgresql://postgres:N6CwvvwtLPJnKRRr@db.cvbsyozqujjjvapqodxj.supabase.co:5432/postgres',
+  'postgresql://postgres:N6CwvvwtLPJnKRRr@db.cvbsyozqujjjvapqodxj.supabase.co:6543/postgres',
+  {
+    max: 1,
+  },
 )
 
 const logger = new Logger()
@@ -10,40 +13,59 @@ const logger = new Logger()
 Deno.serve(async (req) => {
   const { query } = await req.json()
 
+  if (!query) {
+    return new Response(JSON.stringify({ error: 'Query is required' }), {
+      status: 400,
+    })
+  }
+
+  if (typeof query !== 'string') {
+    return new Response(JSON.stringify({ error: 'Query must be a string' }), {
+      status: 400,
+    })
+  }
+
   let end = 0
   const start = performance.now()
-  const { data, error } = await sql
-    .unsafe(query)
-    .then((res) => {
-      end = performance.now()
-      return { data: res, error: null }
-    })
-    .catch((error) => {
-      end = performance.now()
-      logger.error(error)
-      if (error instanceof postgres.PostgresError) {
-        return {
-          data: [],
-          error: {
-            name: error.name,
-            message: error.message,
-            position: error.position,
-          },
-        }
-      }
-      return {
-        data: [],
-        error: new Error('Internal server error'),
-      }
-    })
-    .finally(async () => {
-      // Close the database connection
-      await sql.end()
-    })
-  const executionTime = end - start
-  logger.info(`Query took ${executionTime}ms`)
+  try {
+    const result = await sql.unsafe(query)
+    end = performance.now()
 
-  return new Response(JSON.stringify({ data, error, executionTime }), {
-    headers: { 'Content-Type': 'application/json' },
-  })
+    const executionTime = end - start
+    logger.info(`Query took ${executionTime}ms`)
+
+    return new Response(
+      JSON.stringify({ data: result, error: null, executionTime }),
+      {
+        headers: { 'Content-Type': 'application/json' },
+        status: 200,
+      },
+    )
+  } catch (error) {
+    end = performance.now()
+    logger.error(error)
+    const executionTime = end - start
+    let errorResponse = null
+    if (error instanceof postgres.PostgresError) {
+      errorResponse = {
+        data: [],
+        error: {
+          name: error.name,
+          message: error.message,
+          position: error.position,
+        },
+        executionTime,
+      }
+    } else {
+      errorResponse = {
+        data: [],
+        error: new Error('Internal Server Error'),
+        executionTime,
+      }
+    }
+
+    return new Response(JSON.stringify(errorResponse), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
 })
