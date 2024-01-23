819bfaa4d1330542cc1fd180e2993fe17a620e64
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-23 21:18:19 +0000

    update state

--- app/integrations/app-db/_components/create-table/state.ts
+++ app/integrations/app-db/_components/create-table/state.ts
@@ -4,20 +4,28 @@ type Column = {
   name: string
   type: string
   default: string
+  options: {
+    nullable: boolean
+    unique: boolean
+  }
 }
 
 type Table = {
-  tableName: string
+  name: string
   columns: Column[]
 }
 
 export const createTableAtom = atom<Table>({
-  tableName: '',
+  name: '',
   columns: [
     {
       name: '',
       type: '',
       default: '',
+      options: {
+        nullable: false,
+        unique: false,
+      },
     },
   ],
 })
