78d0cb51a45a46724fcdbb361dcca41b3b8c3e81
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-24 16:50:38 +0000

    move editor and results-table one level up together in a component

--- app/dashboard/[slug]/_components/editor/root.tsx
+++ app/dashboard/[slug]/_components/editor/root.tsx
@@ -3,55 +3,40 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from '@/components/ui/resizable'
-import CodeEditor from './code-editor'
 import { SchemaViewer } from './schema-viewer'
 import { Queries } from './queries'
+import { EditorPanel } from './editor-panel'
 import { Provider } from 'jotai'
-import { Result } from './result'
 
 function Editor() {
   return (
-    <div className="h-full w-full rounded-lg bg-slate-100">
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel minSize={20} defaultSize={20} order={1}>
-          <div className="flex h-full w-full justify-center rounded-lg bg-white">
-            <Queries />
-          </div>
-        </ResizablePanel>
+    <Provider>
+      <div className="h-full w-full rounded-lg bg-slate-100">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel minSize={20} defaultSize={20} order={1}>
+            <div className="flex h-full w-full justify-center rounded-lg bg-white">
+              <Queries />
+            </div>
+          </ResizablePanel>
 
-        <ResizableHandle className="bg-slate-100 p-1" />
+          <ResizableHandle className="bg-slate-100 p-1" />
 
-        <ResizablePanel order={2} defaultSize={60}>
-          <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-white">
-            <Provider>
-              <ResizablePanelGroup
-                autoSaveId="editor-center"
-                direction="vertical"
-              >
-                <ResizablePanel
-                  id="code-editor"
-                  minSize={20}
-                  defaultSize={100}
-                  order={1}
-                >
-                  <CodeEditor />
-                </ResizablePanel>
-                <ResizableHandle />
-                <Result />
-              </ResizablePanelGroup>
-            </Provider>
-          </div>
-        </ResizablePanel>
+          <ResizablePanel order={2} defaultSize={60}>
+            <div className="flex h-full w-full flex-col gap-4 rounded-lg bg-white">
+              <EditorPanel />
+            </div>
+          </ResizablePanel>
 
-        <ResizableHandle className="bg-slate-100 p-1" />
+          <ResizableHandle className="bg-slate-100 p-1" />
 
-        <ResizablePanel minSize={20} defaultSize={20} order={3}>
-          <div className="flex h-full w-full justify-center rounded-lg bg-white">
-            <SchemaViewer />
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
+          <ResizablePanel minSize={20} defaultSize={20} order={3}>
+            <div className="flex h-full w-full justify-center rounded-lg bg-white">
+              <SchemaViewer />
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </Provider>
   )
 }
 
