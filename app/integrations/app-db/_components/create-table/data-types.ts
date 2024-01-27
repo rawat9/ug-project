87a4acd43d907c766035c62eb18857639b3c124b
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-27 10:08:24 +0000

    simplify data types

--- app/integrations/app-db/_components/create-table/data-types.ts
+++ app/integrations/app-db/_components/create-table/data-types.ts
@@ -1,10 +1,8 @@
 export const dataTypes = [
-  { name: 'int4', description: 'Signed 4 byte integer' },
-  { name: 'int8', description: 'Signed 8 byte integer' },
   { name: 'text', description: 'Variable-length character string' },
-  { name: 'numeric', description: 'Exact numeric of selectable precision' },
-  { name: 'varchar', description: 'Variable-length character string' },
+  { name: 'integer', description: 'Exact numeric or floating point decimals' },
   { name: 'date', description: 'Calendar date (year, month, day)' },
+  { name: 'timestamp', description: 'Data and time (no timezone)' },
   { name: 'uuid', description: 'Universally unique identifier' },
   { name: 'bool', description: 'Logical boolean (true/false)' },
 ] as const
