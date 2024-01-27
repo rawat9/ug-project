3af5766cf02e6f7ca57d4949f026821f0db0d23a
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-01-27 10:05:50 +0000

    move types into its own file

--- app/integrations/app-db/_components/create-table/state.ts
+++ app/integrations/app-db/_components/create-table/state.ts
@@ -1,34 +1,8 @@
 import { atom } from 'jotai'
-import { dataTypes } from './data-types'
-
-export type Column = {
-  name: string
-  type: (typeof dataTypes)[number]['name'] | ''
-  default: string
-  options: {
-    primary: boolean
-    nullable: boolean
-    unique: boolean
-  }
-}
-
-type Table = {
-  name: string
-  columns: Column[]
-}
+import type { Table } from './types'
 
 export const createTableAtom = atom<Table>({
   name: '',
-  columns: [
-    {
-      name: '',
-      type: '',
-      default: '',
-      options: {
-        primary: false,
-        nullable: false,
-        unique: false,
-      },
-    },
-  ],
+  description: '',
+  columns: [],
 })
