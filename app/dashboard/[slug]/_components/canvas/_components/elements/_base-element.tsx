391c76f9a168af54c93520eccbe50d920879d71d
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-02-07 15:44:20 +0000

    add dummy area-chart element

--- app/dashboard/[slug]/_components/canvas/_components/elements/_base-element.tsx
+++ app/dashboard/[slug]/_components/canvas/_components/elements/_base-element.tsx
@@ -1,6 +1,7 @@
 import { Element } from '../../types'
 import { TextElement } from './text-element'
 import { CardElement } from './card-element'
+import { AreaChartElement } from './area-chart-element'
 
 export function BaseElement({ element }: { element: Element }) {
   switch (element.type) {
@@ -8,6 +9,8 @@ export function BaseElement({ element }: { element: Element }) {
       return <TextElement element={element} />
     case 'card':
       return <CardElement element={element} />
+    case 'area-chart':
+      return <AreaChartElement element={element} />
     default:
       return null
   }
--- app/dashboard/[slug]/_components/widgets/_components/area-chart-widget.tsx
+++ app/dashboard/[slug]/_components/widgets/_components/area-chart-widget.tsx
@@ -6,9 +6,17 @@ export function AreaChartWidget() {
     <Button
       variant="outline"
       className="flex h-full w-full cursor-grab flex-col items-center gap-4 rounded border p-4"
+      draggable={true}
+      unselectable="on"
+      onDragStart={(e) => {
+        e.dataTransfer.setData('text/plain', '')
+        e.dataTransfer.setData('type', 'area-chart')
+        e.dataTransfer.setData('width', '5')
+        e.dataTransfer.setData('height', '4')
+      }}
     >
       <Chart className="h-10 w-10" />
-      <p>Chart</p>
+      <p>Area Chart</p>
     </Button>
   )
 }
