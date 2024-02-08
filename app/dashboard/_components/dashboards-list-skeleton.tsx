bff518499c8e5b511ec693f2d0712f3273b35113
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-02-08 09:20:41 +0000

    save a line

--- app/dashboard/_components/dashboards-list-skeleton.tsx
+++ app/dashboard/_components/dashboards-list-skeleton.tsx
@@ -4,10 +4,7 @@ export function DashboardsListSkeleton() {
   return (
     <div className="grid gap-4 py-10 md:grid-cols-2 lg:grid-cols-3">
       {Array.from({ length: 3 }).map((_, index) => (
-        <Skeleton
-          key={index}
-          className="min-h-[150px] rounded-lg p-4"
-        ></Skeleton>
+        <Skeleton key={index} className="min-h-[150px] rounded-lg p-4" />
       ))}
     </div>
   )
