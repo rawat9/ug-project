c810e5541f35228bd418f5e76b3b964541225b28
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-02-08 23:33:05 +0000

    WIP

--- app/dashboard/[slug]/_components/properties/root.tsx
+++ app/dashboard/[slug]/_components/properties/root.tsx
@@ -1,46 +1,36 @@
-import { Cross } from '@/icons'
+// import { Cross } from '@/icons'
 import { useCanvasAtom } from '../canvas/state'
 import { Element } from '../canvas/types'
 import { TextElementProperties } from './_components/text-element-properties'
-import { AnimatePresence, motion } from 'framer-motion'
 
 export function Properties() {
-  const { selectedElement, setSelectedElement } = useCanvasAtom()
-
-  if (!selectedElement) {
-    return null
-  }
+  const { selectedElement } = useCanvasAtom()
 
   return (
-    <AnimatePresence>
-      {selectedElement && (
-        <motion.div
-          className="absolute right-0 h-full w-[350px] bg-white shadow-lg"
-          initial={{ opacity: 0, x: '100%' }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: '-100%' }}
-          transition={{
-            type: 'tween',
-            duration: 0.2,
-          }}
+    <div className="h-full">
+      <div className="flex h-[5%] items-center justify-between p-2">
+        <h2 className="text-lg font-semibold">Properties</h2>
+        {/* <button
+          onClick={() => setSelectedElement(null)}
+          className="text-gray-400 hover:text-gray-600"
         >
-          <div className="flex items-center justify-between p-2">
-            <h2 className="text-lg font-semibold">Properties</h2>
-            <button
-              onClick={() => setSelectedElement(null)}
-              className="text-gray-400 hover:text-gray-600"
-            >
-              <Cross className="h-5 w-5" />
-            </button>
-          </div>
-          <div className="h-px w-full bg-gray-200" />
+          <Cross className="h-5 w-5" />
+        </button> */}
+      </div>
+      <div className="h-px w-full bg-gray-200" />
+      {selectedElement ? (
+        <>
           <div className="border-b p-2">{selectedElement.name}</div>
           <div className="px-3 py-4">
             <BaseProperties element={selectedElement} />
           </div>
-        </motion.div>
+        </>
+      ) : (
+        <div className="flex h-[90%] flex-col items-center justify-center p-2 text-gray-500">
+          Select an element to edit its properties
+        </div>
       )}
-    </AnimatePresence>
+    </div>
   )
 }
 
