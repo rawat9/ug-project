704fb0674ec93629aa3a92f52f9fc2f4ff498b26
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-02-17 17:43:48 +0000

    add radio-group component

--- app/dashboard/[slug]/_components/canvas/_components/elements/_base-element.tsx
+++ app/dashboard/[slug]/_components/canvas/_components/elements/_base-element.tsx
@@ -2,16 +2,26 @@ import { Element } from '../../types'
 import { TextElement } from './text-element'
 import { CardElement } from './card-element'
 import { AreaChartElement } from './area-chart-element'
+import { forwardRef } from 'react'
 
-export function BaseElement({ element }: { element: Element }) {
-  switch (element.type) {
-    case 'text':
-      return <TextElement element={element} />
-    case 'card':
-      return <CardElement element={element} />
-    case 'area-chart':
-      return <AreaChartElement element={element} />
-    default:
-      return null
-  }
+interface BaseElementProps {
+  element: Element
 }
+
+const BaseElement = forwardRef<HTMLDivElement, BaseElementProps>(
+  ({ element }, ref) => {
+    switch (element.type) {
+      case 'text':
+        return <TextElement element={element} />
+      case 'card':
+        return <CardElement element={element} />
+      case 'area-chart':
+        return <AreaChartElement element={element} />
+      default:
+        return null
+    }
+  },
+)
+
+BaseElement.displayName = 'BaseElement'
+export { BaseElement }
