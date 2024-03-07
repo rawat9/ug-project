ce1c7f678ab6e58afee5857fc534f4f0eb47a511
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-03-07 23:18:43 +0000

    add animation to the widgets

--- app/dashboard/[slug]/_components/widgets/_components/card-widget.tsx
+++ app/dashboard/[slug]/_components/widgets/_components/card-widget.tsx
@@ -5,7 +5,7 @@ export function CardWidget() {
   return (
     <Button
       variant="outline"
-      className="flex h-full w-full cursor-grab flex-col items-center gap-4 rounded border p-4"
+      className="group flex h-full w-full cursor-grab flex-col items-center gap-4 rounded-xl border p-4"
       draggable={true}
       unselectable="on"
       onDragStart={(e) => {
@@ -15,7 +15,7 @@ export function CardWidget() {
         e.dataTransfer.setData('height', '4')
       }}
     >
-      <Card className="h-10 w-10" />
+      <Card className="h-10 w-10 duration-500 ease-in-out group-hover:scale-125" />
       <p>Card</p>
     </Button>
   )
--- app/dashboard/[slug]/_components/widgets/_components/text-widget.tsx
+++ app/dashboard/[slug]/_components/widgets/_components/text-widget.tsx
@@ -5,17 +5,17 @@ export function TextWidget() {
   return (
     <Button
       variant="outline"
-      className="flex h-full w-full cursor-grab flex-col items-center gap-4 rounded border p-4"
+      className="group flex h-full w-full cursor-grab flex-col items-center gap-4 rounded-xl border p-4"
       draggable={true}
       unselectable="on"
       onDragStart={(e) => {
         e.dataTransfer.setData('text/plain', '')
         e.dataTransfer.setData('type', 'text')
         e.dataTransfer.setData('width', '2')
-        e.dataTransfer.setData('height', '1')
+        e.dataTransfer.setData('height', '2')
       }}
     >
-      <Text className="h-10 w-10" />
+      <Text className="h-10 w-10 duration-500 ease-in-out group-hover:scale-125" />
       <p>Text</p>
     </Button>
   )
