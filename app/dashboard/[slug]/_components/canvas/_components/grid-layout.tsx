e284f86c4d37e62a4509e70f88f1c869809e938a
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-03-13 18:13:22 +0000

    remove isBounded

--- app/dashboard/[slug]/_components/canvas/_components/grid-layout.tsx
+++ app/dashboard/[slug]/_components/canvas/_components/grid-layout.tsx
@@ -14,7 +14,6 @@ export function GridLayout(props: ReactGridLayoutProps) {
       cols={12}
       rowHeight={30}
       resizeHandles={['s', 'w', 'e', 'n', 'sw', 'nw', 'se', 'ne']}
-      // isBounded={true} // Enable this once this issue is merged PR- https://github.com/react-grid-layout/react-grid-layout/pull/2017
       isDroppable={true}
       preventCollision={true}
       compactType={null}
