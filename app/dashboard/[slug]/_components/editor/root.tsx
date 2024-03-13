734b98b92323f8519a589dd896517013f471cf43
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-03-13 17:53:42 +0000

    update the editor toolbar

--- app/dashboard/[slug]/_components/editor/root.tsx
+++ app/dashboard/[slug]/_components/editor/root.tsx
@@ -17,46 +17,48 @@ export function Editor({ isOpen }: { isOpen: boolean }) {
         <div className="absolute bottom-0 z-50 block h-[360px] w-full border-t bg-white shadow-[0px_-2px_5px_-3px_rgba(0,0,0,0.1)]">
           <Provider>
             <div className="flex h-9 w-full items-center border-b px-2">
-              <Button className="h-7 px-1" variant="ghost">
-                <AddCircle className="mr-1 h-5 w-5" />
-                Create new
-              </Button>
-              <TabGroup className="w-[90%] px-2" defaultIndex={0}>
-                <TabList
-                  variant="line"
-                  className="no-scrollbar space-x-0 overflow-y-hidden overflow-x-scroll scroll-smooth border-x border-b-0"
-                >
-                  {Array.from({ length: 5 }).map((_, i) => (
-                    <div
-                      key={i}
-                      className="flex select-none border-l first:border-l-0"
-                    >
-                      <Tab
-                        value="idOfTheQuery"
-                        icon={Postgres}
-                        className="group hover:shadow-inner"
+              <div className="flex w-[85%] items-center">
+                <Button className="h-7 px-1" variant="ghost">
+                  <AddCircle className="mr-1 h-5 w-5" />
+                  Create new
+                </Button>
+                <TabGroup className="px-2" defaultIndex={0}>
+                  <TabList
+                    variant="line"
+                    className="no-scrollbar space-x-0 overflow-y-hidden overflow-x-scroll scroll-smooth border-b-0 border-l"
+                  >
+                    {Array.from({ length: 5 }).map((_, i) => (
+                      <div
+                        key={i}
+                        className="flex select-none border-l first:border-l-0 last:border-r"
                       >
-                        <div className="flex gap-1">
-                          <p>getAllTopics</p>
-                          <Cross
-                            className="h-5 w-5 p-1 text-slate-700 opacity-0 group-hover:opacity-100 group-aria-selected:opacity-100"
-                            onClick={() => {
-                              console.log('delete')
-                            }}
-                          />
-                        </div>
-                      </Tab>
-                    </div>
-                  ))}
-                </TabList>
-              </TabGroup>
+                        <Tab
+                          value="idOfTheQuery"
+                          icon={Postgres}
+                          className="group hover:shadow-inner ui-selected:border-slate-600 ui-selected:text-slate-800"
+                        >
+                          <div className="flex gap-1">
+                            <p>getAllTopics</p>
+                            <Cross
+                              className="h-5 w-5 p-1 text-slate-700 opacity-0 group-hover:opacity-100 group-aria-selected:opacity-100"
+                              onClick={() => {
+                                console.log('delete')
+                              }}
+                            />
+                          </div>
+                        </Tab>
+                      </div>
+                    ))}
+                  </TabList>
+                </TabGroup>
+              </div>
 
               <TooltipProvider delayDuration={0}>
                 <Tooltip>
                   <TooltipTrigger asChild>
-                    <Button className="ml-auto h-7" variant="ghost">
+                    <button className="ml-auto p-1">
                       <PanelBottom className="h-6 w-6 text-slate-500 hover:text-slate-700" />
-                    </Button>
+                    </button>
                   </TooltipTrigger>
                   <TooltipContent side="bottom">
                     <p>Toggle editor</p>
