49f5ac676ecf5c242e057cade3d3e525fff97584
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-03-15 23:28:55 +0000

    move animation back to component level

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   ResizableHandle,
   ResizablePanel,
@@ -11,28 +9,22 @@ import { Canvas } from './_components/canvas'
 import { Widgets } from './_components/widgets'
 import { Properties } from './_components/properties'
 import { State } from './_components/state'
-import { AnimatePresence, motion } from 'framer-motion'
 
 import { elementsAtom } from './_components/canvas/state'
 import { saveCanvas } from '@/lib/data'
 import { useAutosave } from '@/hooks'
-import { useAtomValue } from 'jotai'
-import { usePathname, useRouter } from 'next/navigation'
+import { Provider, useAtomValue } from 'jotai'
 
 export default function Page({
-  params,
   searchParams,
 }: {
-  params: { slug: string }
   searchParams?: {
     editor?: string
     widgets?: string
     state?: string
   }
 }) {
-  const param = new URLSearchParams(searchParams)
-  const { replace } = useRouter()
-  const pathname = usePathname()
+  // const pathname = usePathname()
   // const elements = useAtomValue(elementsAtom)
 
   // const handleSave = async () => {
@@ -52,37 +44,8 @@ export default function Page({
       autoSaveId="persistance"
       className="fixed left-12 top-14 z-10 max-h-[calc(100vh_-_3.5rem)] max-w-[calc(100vw_-_3rem)]"
     >
-      {(searchParams?.state || searchParams?.widgets) && (
-        <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0, x: '-100%' }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: '-100%' }}
-            transition={{
-              type: 'tween',
-              duration: 0.2,
-            }}
-            className="absolute left-2 top-2 z-50 block h-[550px] w-[300px] rounded-md bg-white shadow-md"
-          >
-            {searchParams?.widgets === 'true' && (
-              <Widgets
-                onCollapse={() => {
-                  param.delete('widgets')
-                  replace(`${pathname}?${param.toString()}`, { scroll: false })
-                }}
-              />
-            )}
-            {searchParams?.state === 'true' && (
-              <State
-                onCollapse={() => {
-                  param.delete('state')
-                  replace(`${pathname}?${param.toString()}`, { scroll: false })
-                }}
-              />
-            )}
-          </motion.div>
-        </AnimatePresence>
-      )}
+      <Widgets isOpen={searchParams?.widgets === 'true'} />
+      <State isOpen={searchParams?.state === 'true'} />
       <ResizablePanel
         id="canvas"
         defaultSize={80}
@@ -91,7 +54,9 @@ export default function Page({
         className="relative"
       >
         <Canvas />
-        <Editor isOpen={searchParams?.editor === 'true'} />
+        <Provider>
+          <Editor isOpen={searchParams?.editor === 'true'} />
+        </Provider>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel
